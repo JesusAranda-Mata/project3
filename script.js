7a93628d02747a143c6e902b2e625e3c94a92498
Jesus <jesus.aranda7@yahoo.com>
2020-01-30 09:36:20 -0600

    final submit of message in/out

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const sentBtn = document.querySelector('#sendBtn');
 const massageIn = document.querySelector('#massegeIn');
 const massageOut = document.querySelector('#massageOut');
 
+sendBtn.addEventListener('click', sendMsg)
+
+function sendMsg (){
+    let content = messageIn.value;
+    //console.log(content);
+    if(content === '') {
+        alert('Please Enter Valid Value. Current Value Is Empty')
+    } 
+else {
+    messageOut.innerHTML = content;
+    messageIn.value = '';
+  }
+    
+}
